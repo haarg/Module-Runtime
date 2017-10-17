84f3149899f9812ac52c559e6706df71f33e46f0
Zefram <zefram@fysh.org>
2017-10-17 21:20:34 +0100

    new version

--- lib/Module/Runtime.pm
+++ lib/Module/Runtime.pm
@@ -117,7 +117,7 @@ BEGIN { require 5.006; }
 BEGIN { ${^WARNING_BITS} = ""; }
 # Don't "use strict" here, to avoid dependencies.
 
-our $VERSION = "0.015";
+our $VERSION = "0.016";
 
 # Don't use Exporter here, to avoid dependencies.
 our @EXPORT_OK = qw(
